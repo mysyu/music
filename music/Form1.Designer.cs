d336e32d2d089962e18022b960b035bcb2656dfe
s1033349 <j4682739150@yahoo.com.tw>
2017-03-09 13:41:00 +0800

    upload

--- music/Form1.Designer.cs
+++ music/Form1.Designer.cs
@@ -28,13 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(799, 468);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +59,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- music/Form1.cs
+++ music/Form1.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Text;
 
 namespace music
 {
@@ -15,6 +11,44 @@ namespace music
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click( object sender , EventArgs e )
+        {
+            Stream musicStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "wav files (*.wav)|*.wav|mp3 files (*.mp3)|*.mp3";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if ( openFileDialog1.ShowDialog() == DialogResult.OK )
+            {
+                try
+                {
+                    if ( ( musicStream = openFileDialog1.OpenFile() ) != null )
+                    {
+                        using ( musicStream )
+                        {
+                            FtpWebRequest request = (FtpWebRequest) WebRequest.Create( @"ftp:\\1.34.30.96:21\" );
+                            request.Method = WebRequestMethods.Ftp.UploadFile;
+                            request.Credentials = new NetworkCredential( "UploadMusic" , "UploadMusic" );
+                            request.ContentLength = musicStream.Length;
+                            using ( Stream ftpStream = request.GetRequestStream() )
+                            {
+                                musicStream.CopyTo( ftpStream );
+                            }
+                            FtpWebResponse response = (FtpWebResponse) request.GetResponse();
+                            Console.WriteLine( "Upload status: {0}, {1}" , response.StatusCode , response.StatusDescription );
+                            response.Close();
+                        }
+                    }
+                }
+                catch ( Exception ex )
+                {
+                    MessageBox.Show( ex.Message );
+                }
+            }
         }
     }
 }
