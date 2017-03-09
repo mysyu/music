3f2b6e4598bbf0a78b69163ebb3951c91324686a
s1033349 <j4682739150@yahoo.com.tw>
2017-03-09 14:18:57 +0800

    music control class

--- music/Form1.cs
+++ music/Form1.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Windows.Forms;
-using System.Net;
-using System.IO;
 
 namespace music
 {
@@ -14,40 +12,7 @@ namespace music
 
         private void button1_Click( object sender , EventArgs e )
         {
-            Stream musicStream = null;
-            OpenFileDialog openFileDialog1 = new OpenFileDialog();
-            openFileDialog1.InitialDirectory = "c:\\";
-            openFileDialog1.Filter = "wav files (*.wav)|*.wav|mp3 files (*.mp3)|*.mp3";
-            openFileDialog1.FilterIndex = 2;
-            openFileDialog1.RestoreDirectory = true;
-
-            if ( openFileDialog1.ShowDialog() == DialogResult.OK )
-            {
-                try
-                {
-                    if ( ( musicStream = openFileDialog1.OpenFile() ) != null )
-                    {
-                        using ( musicStream )
-                        {
-                            FtpWebRequest request = (FtpWebRequest) WebRequest.Create( @"ftp:\\1.34.30.96:21\" );
-                            request.Method = WebRequestMethods.Ftp.UploadFile;
-                            request.Credentials = new NetworkCredential( "UploadMusic" , "UploadMusic" );
-                            request.ContentLength = musicStream.Length;
-                            using ( Stream ftpStream = request.GetRequestStream() )
-                            {
-                                musicStream.CopyTo( ftpStream );
-                            }
-                            FtpWebResponse response = (FtpWebResponse) request.GetResponse();
-                            Console.WriteLine( "Upload status: {0}, {1}" , response.StatusCode , response.StatusDescription );
-                            response.Close();
-                        }
-                    }
-                }
-                catch ( Exception ex )
-                {
-                    MessageBox.Show( ex.Message );
-                }
-            }
+            new MusicControl().Upload();
         }
     }
 }
