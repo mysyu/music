92974bec5523c125822c464b4a0666ce9b988f90
s1033349 <j4682739150@yahoo.com.tw>
2017-03-09 22:52:30 +0800

    mp3

--- music/MusicPlayer.Designer.cs
+++ music/MusicPlayer.Designer.cs
@@ -1,13 +1,13 @@
 ﻿namespace music
 {
-    partial class Form1
+    partial class MusicPlayer
     {
-        /// <summary>
+        /// <summary> 
         /// 設計工具所需的變數。
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
+        /// <summary> 
         /// 清除任何使用中的資源。
         /// </summary>
         /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
@@ -20,21 +20,25 @@
             base.Dispose( disposing );
         }
 
-        #region Windows Form 設計工具產生的程式碼
+        #region 元件設計工具產生的程式碼
 
-        /// <summary>
+        /// <summary> 
         /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
         /// 這個方法的內容。
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
-            this.musicPlayer1 = new music.MusicPlayer();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Location = new System.Drawing.Point(13, 4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -42,30 +46,36 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // musicPlayer1
+            // errorProvider1
             // 
-            this.musicPlayer1.Location = new System.Drawing.Point(24, 73);
-            this.musicPlayer1.Name = "musicPlayer1";
-            this.musicPlayer1.Size = new System.Drawing.Size(438, 167);
-            this.musicPlayer1.TabIndex = 1;
+            this.errorProvider1.ContainerControl = this;
             // 
-            // Form1
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(118, 4);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 2;
+            // 
+            // MusicPlayer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(799, 468);
-            this.Controls.Add(this.musicPlayer1);
+            this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "MusicPlayer";
+            this.Size = new System.Drawing.Size(307, 69);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private MusicPlayer musicPlayer1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.TrackBar trackBar1;
     }
 }
-
---
+++ music/MusicPlayer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Media;
+using NAudio.Wave;
+using WMPLib;
+
+namespace music
+{
+    public partial class MusicPlayer : UserControl
+    {
+        public MusicPlayer()
+        {
+            InitializeComponent();
+        }
+
+        public void Upload()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "audio files ( *.wav , *.mp3 )|*.wav;*.mp3";
+            openFileDialog.FilterIndex = 1;
+            Stream musicStream = null;
+            if ( openFileDialog.ShowDialog() == DialogResult.OK )
+            {
+                try
+                {
+                    if ( ( musicStream = openFileDialog.OpenFile() ) != null )
+                    {
+                        using ( musicStream )
+                        {
+                            {
+                                FtpWebRequest request = (FtpWebRequest) WebRequest.Create( "ftp://1.34.30.96:21/" + Path.GetFileName( openFileDialog.FileName ) );
+                                request.Method = WebRequestMethods.Ftp.UploadFile;
+                                request.Credentials = new NetworkCredential( "UploadMusic" , "UploadMusic" );
+
+                                using ( Stream requestStream = request.GetRequestStream() )
+                                {
+                                    musicStream.CopyTo( requestStream );
+                                }
+
+                                using ( FtpWebResponse response = (FtpWebResponse) request.GetResponse() )
+                                {
+                                    MessageBox.Show( "Upload File Complete, status " + response.StatusDescription );
+                                }
+                            }
+                        }
+                    }
+                }
+                catch ( Exception ex )
+                {
+                    MessageBox.Show( ex.Message );
+                }
+            }
+        }
+        private void button1_Click( object sender , EventArgs e )
+        {
+            try
+            {
+                WindowsMediaPlayer music = new WindowsMediaPlayer();
+                music.URL = "http://mysyu.ddns.net/UploadMusic/IU.mp3";
+                trackBar1.Maximum = (int)music.currentMedia.duration;
+                trackBar1.Enabled = true;
+                music.controls.play();
+            }
+            catch ( Exception ex )
+            {
+                MessageBox.Show( ex.Message );
+            }
+        }
+
+    }
+}
