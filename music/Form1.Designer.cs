25e2a65c5c65eee08ae2072a064bcdf9cd8a3022
s1033349 <j4682739150@yahoo.com.tw>
2017-03-18 21:35:55 +0800

    system

--- music/Form1.Designer.cs
+++ music/Form1.Designer.cs
@@ -148,6 +148,7 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             ((System.ComponentModel.ISupportInitialize)(this.musicPlayer1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
