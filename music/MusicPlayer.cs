98dba991a95eaa88fe80f2a19f5c1c6f683b593c
s1033349 <j4682739150@yahoo.com.tw>
2017-03-23 19:34:10 +0800

    ok

---
+++ music/MusicPlayer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using AxWMPLib;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace music
+{
+    public partial class MusicPlayer : AxWindowsMediaPlayer
+    {
+
+        public MusicPlayer()
+        {
+        }
+        public void Upload()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "audio files ( *.wav , *.mp3 )|*.wav;*.mp3";
+            openFileDialog.FilterIndex = 1;
+            Stream musicStream = null;
+            if ( openFileDialog.ShowDialog() == DialogResult.OK )
+            {
+                try
+                {
+                    if ( ( musicStream = openFileDialog.OpenFile() ) != null )
+                    {
+                        using ( musicStream )
+                        {
+                            {
+                                FtpWebRequest request = (FtpWebRequest) WebRequest.Create( "ftp://1.34.30.96:21/" + Path.GetFileName( openFileDialog.FileName ) );
+                                request.Method = WebRequestMethods.Ftp.UploadFile;
+                                request.Credentials = new NetworkCredential( "UploadMusic" , "UploadMusic" );
+
+                                using ( Stream requestStream = request.GetRequestStream() )
+                                {
+                                    musicStream.CopyTo( requestStream );
+                                }
+
+                                using ( FtpWebResponse response = (FtpWebResponse) request.GetResponse() )
+                                {
+                                    MessageBox.Show( "Upload File Complete, status " + response.StatusDescription );
+                                }
+                            }
+                        }
+                    }
+                }
+                catch ( Exception ex )
+                {
+                    MessageBox.Show( ex.Message );
+                }
+            }
+        }
+
+    }
+}
