315add46663c4cf8160960fcefed67e44b8bd7f6
edward123 <edward123@EDWARD>
2017-04-01 00:54:21 +0800

    lyric

--- music/Music Control/Lyrics.cs
+++ music/Music Control/Lyrics.cs
@@ -17,7 +17,7 @@ namespace music.Music_Control
                 complete = line.Replace("\r", "");
                 foreach (String l in complete.Split('\n'))
                 {
-                    if (l != "")
+                    if (!(String.IsNullOrWhiteSpace(l)))
                     {
                         DateTime t = DateTime.ParseExact(l.Substring(1, 5), "mm:ss", CultureInfo.InvariantCulture);
                         String w = l.Substring(7); 
@@ -31,11 +31,72 @@ namespace music.Music_Control
                         this.words.Add(w);
                         this.time.Add(t);
                     }
+                    else
+                    {
+                        if(this.time.Count <= 0)
+                        {
+                            throw new Exception("First line cannot be space");
+                        }
+                        DateTime t = this.time[this.time.Count - 1];
+                        String w = "";
+                        this.words.Add(w);
+                        this.time.Add(t);
+                    }
                 }
             }
             catch (Exception e)
             {
                 MessageBox.Show(e.Message);
+                throw e;
+            }
+        }
+        public String getTime(int n)
+        {
+            try
+            {
+                if (n > this.time.Count)
+                    throw new Exception("The number is out of range");
+                return this.time[n - 1].ToString("mm:ss");
+                
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                throw e;
+            }
+        }
+        public int getLine(DateTime t)
+        {   try
+            {
+                int line=0;
+                if (t > this.time[this.time.Count-1])
+                    throw new Exception("The time is out of range");
+                for (int i = 0; i < this.time.Count; i++)
+                {
+                    if (this.time[i] > t)
+                    {
+                        if (this.words[i] != "")
+                            line = i + 1;
+                        else
+                        {
+                            for(int j=i-1;j>0;j--)
+                            {
+                                if(this.words[j]!="")
+                                {
+                                    line = j + 1;
+                                    break;
+                                }
+                            }
+                        }
+                        break;
+                    }
+                }
+                return line;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                throw e;
             }
         }
         private String complete;
