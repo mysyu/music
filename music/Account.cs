bd87244e484b231140401832437f9e35f5bb9d93
mike7230 <a1050057@gmail.com>
2017-04-22 21:40:33 +0800

    account

--- music/Account.cs
+++ music/Account.cs
@@ -3,12 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace music
 {
     public static class Account
     {
-        public static bool login = false;
-        public static string name = "";
+        public static bool islogin = false;
+        public static string email = "";
+
+        public static bool Login(string e, string p)
+        {
+            DataTable result = DB.Select(String.Format("Select * from account where email={0}, password={1}", e, p));
+            if (result.Rows.Count == 1)
+            {
+                islogin = true;
+                email = e;
+                return true;
+            }
+            else
+                return false;
+        }
+        public static void logout()
+        {
+            islogin = false;
+            email = "";
+        }
+        public static string regist(string e, string p)
+        {
+            int result = DB.SQL(String.Format("INSERT INTO account(email , password) VALUES ({0},{1})", e, p));
+            if (result == 1)
+                return "Regist Success";
+            else
+                return "Regist Fail";
+        }
     }
+
 }
