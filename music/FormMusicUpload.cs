5ee2fdb957a6251dab59ce932334d062f02c18da
edward123 <edward123@EDWARD>
2017-04-23 16:30:35 +0800

    123

--- music/FormMusicUpload.cs
+++ music/FormMusicUpload.cs
@@ -17,5 +17,24 @@ namespace music
             InitializeComponent();
         }
 
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked == true)
+                checkBox4.Checked = false;
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox4.Checked == true)
+                checkBox3.Checked = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+
+        }
     }
 }
