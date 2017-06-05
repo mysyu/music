11688fa00b7585cd5e001dcd2272855397b2772c
s1033349 <j4682739150@yahoo.com.tw>
2017-06-05 13:30:20 +0800

    home top 5

--- music/FormHome.Designer.cs
+++ music/FormHome.Designer.cs
@@ -28,8 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.tag = new System.Windows.Forms.Button();
             this.table = new System.Windows.Forms.TableLayoutPanel();
+            this.musicList_Option = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.musicInfo = new System.Windows.Forms.ToolStripMenuItem();
+            this.singerInfo = new System.Windows.Forms.ToolStripMenuItem();
+            this.addCurrent = new System.Windows.Forms.ToolStripMenuItem();
+            this.addLocal = new System.Windows.Forms.ToolStripMenuItem();
+            this.addAccount = new System.Windows.Forms.ToolStripMenuItem();
+            this.play = new System.Windows.Forms.ToolStripMenuItem();
+            this.musicList_Option.SuspendLayout();
             this.SuspendLayout();
             // 
             // tag
@@ -51,9 +60,64 @@
             this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.table.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.table.Location = new System.Drawing.Point(0, 85);
+            this.table.Location = new System.Drawing.Point(0, 461);
             this.table.Name = "table";
+            this.table.Size = new System.Drawing.Size(501, 0);
             this.table.TabIndex = 6;
+            // 
+            // musicList_Option
+            // 
+            this.musicList_Option.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.musicInfo,
+            this.singerInfo,
+            this.addCurrent,
+            this.addLocal,
+            this.addAccount,
+            this.play});
+            this.musicList_Option.Name = "musicList_Option";
+            this.musicList_Option.Size = new System.Drawing.Size(147, 136);
+            // 
+            // musicInfo
+            // 
+            this.musicInfo.Name = "musicInfo";
+            this.musicInfo.Size = new System.Drawing.Size(152, 22);
+            this.musicInfo.Text = "歌曲資訊";
+            this.musicInfo.Click += new System.EventHandler(this.musicInfo_Click);
+            // 
+            // singerInfo
+            // 
+            this.singerInfo.Name = "singerInfo";
+            this.singerInfo.Size = new System.Drawing.Size(152, 22);
+            this.singerInfo.Text = "創作者資訊";
+            this.singerInfo.Click += new System.EventHandler(this.singerInfo_Click);
+            // 
+            // addCurrent
+            // 
+            this.addCurrent.Name = "addCurrent";
+            this.addCurrent.Size = new System.Drawing.Size(152, 22);
+            this.addCurrent.Text = "加入撥放歌單";
+            this.addCurrent.Click += new System.EventHandler(this.addCurrent_Click);
+            // 
+            // addLocal
+            // 
+            this.addLocal.Name = "addLocal";
+            this.addLocal.Size = new System.Drawing.Size(152, 22);
+            this.addLocal.Text = "加入本機歌單";
+            this.addLocal.Click += new System.EventHandler(this.addLocal_Click);
+            // 
+            // addAccount
+            // 
+            this.addAccount.Name = "addAccount";
+            this.addAccount.Size = new System.Drawing.Size(152, 22);
+            this.addAccount.Text = "加入帳戶歌單";
+            this.addAccount.Click += new System.EventHandler(this.addAccount_Click);
+            // 
+            // play
+            // 
+            this.play.Name = "play";
+            this.play.Size = new System.Drawing.Size(152, 22);
+            this.play.Text = "撥放";
+            this.play.Click += new System.EventHandler(this.play_Click);
             // 
             // FormHome
             // 
@@ -62,11 +126,12 @@
             this.AutoScroll = true;
             this.AutoScrollMargin = new System.Drawing.Size(0, 50);
             this.ClientSize = new System.Drawing.Size(501, 461);
-            this.Controls.Add(this.table);
             this.Controls.Add(this.tag);
+            this.Controls.Add(this.table);
             this.Name = "FormHome";
             this.Text = "FormHome";
             this.Shown += new System.EventHandler(this.FormHome_Shown);
+            this.musicList_Option.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -76,5 +141,12 @@
 
         public System.Windows.Forms.Button tag;
         private System.Windows.Forms.TableLayoutPanel table;
+        private System.Windows.Forms.ContextMenuStrip musicList_Option;
+        private System.Windows.Forms.ToolStripMenuItem musicInfo;
+        private System.Windows.Forms.ToolStripMenuItem singerInfo;
+        private System.Windows.Forms.ToolStripMenuItem addCurrent;
+        private System.Windows.Forms.ToolStripMenuItem addLocal;
+        private System.Windows.Forms.ToolStripMenuItem addAccount;
+        private System.Windows.Forms.ToolStripMenuItem play;
     }
 }
--- music/FormHome.cs
+++ music/FormHome.cs
@@ -14,6 +14,8 @@ namespace music
     {
         private List<Panel> panels = null;
         private List<String> tags = null;
+        private Music select = null;
+
         public FormHome()
         {
             InitializeComponent();
@@ -26,12 +28,12 @@ namespace music
             formMusicList.FormBorderStyle = FormBorderStyle.None;
             FormMain.main.mainPanel.Controls.Add( formMusicList );
             formMusicList.BringToFront();
-            formMusicList.Set( null , false );
             formMusicList.Show();
         }
 
         private void FormHome_Shown( object sender , EventArgs e )
         {
+            table.Top = tag.Bottom;
             tags = new List<string>();
             tags.Add( "所有類型" );
             tags.Add( "原創" );
@@ -63,12 +65,102 @@ namespace music
         {
             for ( int i = 0 ; i < tags.Count ; i++ )
             {
+                int top = 5;
                 panels[ i ].Controls.Clear();
-                Label l = new Label();
-                l.Text = tags[ i ] + " Top 10";
-                l.Location = new Point( 5 , 5 );
-                panels[ i ].Controls.Add( l );
+                Label label = new Label();
+                label.Text = tags[ i ] + " Top 5";
+                label.Location = new Point( 5 , top );
+                panels[ i ].Controls.Add( label );
+                LinkLabel link = null;
+                foreach( Music m in MusicList.search( String.Format( "Select ID from uploadmusic where 1 = 0 or tag like '%{0}%' order by listen desc" , tags[ i ] == "所有類型" ? "" : tags[ i ] ) ) )
+                {
+                    top += 30;
+                    link = new LinkLabel();
+                    link.Text = m.name;
+                    link.Tag = (Object) m;
+                    link.LinkClicked += new LinkLabelLinkClickedEventHandler( click );
+                    link.Location = new Point( 5 , top );
+                    panels[ i ].Controls.Add( link );
+
+                }
+                top += 30;
+                link = new LinkLabel();
+                link.Text = "更多";
+                link.Tag = (Object) tags[i] == "所有類型" ? "原創;翻唱" : tags[ i ];
+                link.LinkClicked += new LinkLabelLinkClickedEventHandler( more );
+                link.Location = new Point( 5 , top );
+                panels[ i ].Controls.Add( link );
             }
+        }
+
+        private void more( object sender , LinkLabelLinkClickedEventArgs e )
+        {
+            LinkLabel link = (LinkLabel) sender;
+            FormMusicList formMusicList = new FormMusicList();
+            formMusicList.TopLevel = false;
+            formMusicList.Dock = DockStyle.Fill;
+            formMusicList.FormBorderStyle = FormBorderStyle.None;
+            FormMain.main.mainPanel.Controls.Add( formMusicList );
+            formMusicList.BringToFront();
+
+            foreach( CheckBox c in formMusicList.tag.Controls.OfType<CheckBox>().Where( r => ((String)link.Tag).Contains( r.Text ) ) )
+            {
+                c.Checked = true;
+            }
+            formMusicList.Show();
+        }
+        private void click( object sender , LinkLabelLinkClickedEventArgs e )
+        {
+            LinkLabel link = (LinkLabel) sender;
+            select = (Music) link.Tag;
+            addAccount.Visible = Account.islogin;
+            musicList_Option.Show( link , new Point( link.Width - musicList_Option.Width , link.Height ) );
+        }
+        private void musicInfo_Click( object sender , EventArgs e )
+        {
+            FormMusicInfo formMusicInfo = new FormMusicInfo();
+            formMusicInfo.TopLevel = false;
+            formMusicInfo.Dock = DockStyle.Fill;
+            formMusicInfo.FormBorderStyle = FormBorderStyle.None;
+            FormMain.main.mainPanel.Controls.Add( formMusicInfo );
+            formMusicInfo.BringToFront();
+            formMusicInfo.Set( select );
+            select = null;
+            formMusicInfo.Show();
+        }
+        private void singerInfo_Click( object sender , EventArgs e )
+        {
+            FormMusicSinger formMusicSinger = new FormMusicSinger();
+            formMusicSinger.TopLevel = false;
+            formMusicSinger.Dock = DockStyle.Fill;
+            formMusicSinger.FormBorderStyle = FormBorderStyle.None;
+            FormMain.main.mainPanel.Controls.Add( formMusicSinger );
+            formMusicSinger.BringToFront();
+            formMusicSinger.Set( new Account( select ) );
+            select = null;
+            formMusicSinger.Show();
+        }
+        private void addCurrent_Click( object sender , EventArgs e )
+        {
+            MusicList.add( select , false );
+            select = null;
+        }
+        private void addLocal_Click( object sender , EventArgs e )
+        {
+            MusicList.add( "local" , select );
+            FormMain.main.refreshMusiclist();
+            select = null;
+        }
+        private void addAccount_Click( object sender , EventArgs e )
+        {
+            MusicList.add( "account" , select );
+            FormMain.main.refreshMusiclist();
+            select = null;
+        }
+        private void play_Click( object sender , EventArgs e )
+        {
+            MusicList.add( select , true );
+            select = null;
         }
     }
 }
