66c38fd481ed46080675b2b7a6795002c15db25a
s1033349 <j4682739150@yahoo.com.tw>
2017-06-05 09:34:09 +0800

    home

--- music/FormHome.Designer.cs
+++ music/FormHome.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.tag = new System.Windows.Forms.Button();
+            this.table = new System.Windows.Forms.TableLayoutPanel();
             this.SuspendLayout();
             // 
             // tag
@@ -41,20 +42,39 @@
             this.tag.UseVisualStyleBackColor = true;
             this.tag.Click += new System.EventHandler(this.tag_Click);
             // 
+            // table
+            // 
+            this.table.AutoSize = true;
+            this.table.ColumnCount = 4;
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.table.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.table.Location = new System.Drawing.Point(0, 85);
+            this.table.Name = "table";
+            this.table.TabIndex = 6;
+            // 
             // FormHome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.AutoScroll = true;
+            this.AutoScrollMargin = new System.Drawing.Size(0, 50);
+            this.ClientSize = new System.Drawing.Size(501, 461);
+            this.Controls.Add(this.table);
             this.Controls.Add(this.tag);
             this.Name = "FormHome";
             this.Text = "FormHome";
+            this.Shown += new System.EventHandler(this.FormHome_Shown);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         public System.Windows.Forms.Button tag;
+        private System.Windows.Forms.TableLayoutPanel table;
     }
 }
--- music/FormHome.cs
+++ music/FormHome.cs
@@ -12,6 +12,8 @@ namespace music
 {
     public partial class FormHome : Form
     {
+        private List<Panel> panels = null;
+        private List<String> tags = null;
         public FormHome()
         {
             InitializeComponent();
@@ -26,6 +28,47 @@ namespace music
             formMusicList.BringToFront();
             formMusicList.Set( null , false );
             formMusicList.Show();
+        }
+
+        private void FormHome_Shown( object sender , EventArgs e )
+        {
+            tags = new List<string>();
+            tags.Add( "所有類型" );
+            tags.Add( "原創" );
+            tags.Add( "翻唱" );
+            tags.Add( "純音樂" );
+            tags.Add( "重金屬" );
+            tags.Add( "搖滾" );
+            tags.Add( "爵士" );
+            tags.Add( "輕音樂" );
+            tags.Add( "流行音樂" );
+            table.Controls.Clear();
+            table.RowCount = (int) Math.Ceiling( tags.Count / 4.0 );
+            panels = new List<Panel>();
+            for ( int i = 0 ; i < table.RowCount ; i++ )
+            {
+                table.RowStyles.Add( new System.Windows.Forms.RowStyle( System.Windows.Forms.SizeType.Absolute , 250F ) );
+                for ( int j = 0 ; j < table.ColumnCount ; j++ )
+                {
+                    Panel panel = new Panel();
+                    panel.Dock = DockStyle.Fill;
+                    panels.Add( panel );
+                    table.Controls.Add( panel , j , i );
+                }
+            }
+            Set();
+        }
+
+        private void Set()
+        {
+            for ( int i = 0 ; i < tags.Count ; i++ )
+            {
+                panels[ i ].Controls.Clear();
+                Label l = new Label();
+                l.Text = tags[ i ] + " Top 10";
+                l.Location = new Point( 5 , 5 );
+                panels[ i ].Controls.Add( l );
+            }
         }
     }
 }
